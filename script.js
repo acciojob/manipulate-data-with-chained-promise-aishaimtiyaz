40bf5c8ef1a88d23664a61fe88f626e98afe7dbc
aishaimtiyaz <144771688+aishaimtiyaz@users.noreply.github.com>
2023-12-14 00:10:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const arr=[1, 2, 3 ,4];
+const out= document.getElementById('output');
+
+ const promi= new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+
+          const odd= arr.filter((item)=>{
+            if(item % 2 == 0){
+              return item;
+            }
+        })
+	     out.innerHtml=odd;
+        }, 1000)
+
+        setTimeout(()=>{
+          const even= arr.filter((item)=>{
+            if(item%2 !=0){
+              return item
+            }
+          })
+         out.innerHtml=even;
+          resolve()
+          console.log(promi)
+        }, 3000)
+    })
